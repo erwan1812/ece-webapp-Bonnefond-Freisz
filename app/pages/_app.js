bf86300ee270f80c525b4ecb30da94c5d10a38f5
erwan1812 <78738801+erwan1812@users.noreply.github.com>
2022-12-12 13:52:14 +0100

    refactor: home page & _app

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,23 +1,26 @@
 import '../styles/globals.css'
-
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
-
 import Layout from '../components/Layout'
-import {ContextProvider} from '../components/UserContext'
+import { ContextProvider } from '../components/UserContext'
+import { ThemeProvider } from "next-themes";
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
   return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-    <ContextProvider>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    </ContextProvider>
+   <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+      <ThemeProvider enableSystem={true} attribute="class">
+
+        <ContextProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ContextProvider>
+
+      </ThemeProvider>
     </SessionContextProvider>
-    
+
   )
 }
 
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,25 +1,25 @@
-import Link from 'next/link'
 import Head from 'next/head'
 
 
-function Home() {
+export default function Home({ articles }) {
+  console.log(articles)
   return (
     <>
       <Head>
         <title>My app</title>
       </Head>
-      <body>
+      <div class="dark:bf-gray-900">
         <div class="h-20 w-1OO bg-orange-300 grid-cols-3 gap-4 content-center">
           <div class="text-center mx-4 space-y-2 py-4">
-            <div class="text-black-600 text-5xl font-bold ">
+            <div class="dark:text-violet-600 text-black-600 text-5xl font-bold ">
               ACCUEIL
             </div>
           </div>
         </div>
-      </body>
+      </div>
 
     </>
   )
 }
 
-export default Home
+
