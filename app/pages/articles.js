d5c39d9868067ea2e719ba30c8cd2a3ccc0f1834
erwan1812 <78738801+erwan1812@users.noreply.github.com>
2022-12-12 13:48:39 +0100

    feat: supabase & articles

--- app/pages/articles.js
+++ app/pages/articles.js
@@ -1,36 +1,31 @@
 import Link from "next/link";
 import React, { useState } from "react";
+import { supabase } from '../utils/supabase'
 
-function articles({ posts }) {
-  const [listOfPost, setListOfPost] = useState(posts);
+function articles({ articles }) {
 
-  const loadMore=async() => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10&&_start=10");
-  const posts = await res.json();
-  setListOfPost((value) => [...value, ...posts]);
-  }
   return (
-    <ul>
-      {listOfPost.map((post, blog) => {
-        return (
-        <Link href={`../posts/${post.id}`} key={blog} passHref> 
-          <h2 key={blog}>
-            <li>{post.id}.{post.title}</li>
-          </h2>
-          </Link> 
-        );
-      })}
-      <button onClick={loadMore}>Load more</button>
-    </ul>
-    
+    <>
+   <div className="w-full max-w-3xl mx-auto my-16 px-2">
+          {articles.map((article) => (
+            <Link key={article.id} href={`/${article.id}`}>
+              <a className="p-8 h-40 mb-4 rounded shadow text-xl flex">
+                {article.title}
+              </a>
+              </Link>
+          ))}
+        </div>
+    </>
   )
 }
 
-export async function getStaticProps(context) {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
-  const posts = await res.json();
+
+export const getStaticProps = async () => {
+  const {data: articles} = await supabase.from('article').select('*')
   return {
-    props: { posts },
+    props: {
+      articles
+    }
   }
 }
 export default articles
