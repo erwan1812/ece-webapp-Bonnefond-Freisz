7a2ece70075dc5275257b861833ba0077a101d1e
erwan1812 <78738801+erwan1812@users.noreply.github.com>
2022-12-22 15:00:17 +0100

    fix: deployment (error:lint)

--- app/pages/index.js
+++ app/pages/index.js
@@ -9,14 +9,14 @@ export default function Home({ articles }) {
       <Head>
         <title>WebApp</title>
       </Head>
-      <div class="h-20 w-1OO grid-cols-3 gap-4 content-center mt-5">
-        <div class="text-center mx-4 space-y-2 py-4">
-          <div class="dark:text-violet-600 text-black-200 text-5xl font-serif">
+      <div className="h-20 w-1OO grid-cols-3 gap-4 content-center mt-5">
+        <div className="text-center mx-4 space-y-2 py-4">
+          <div className="dark:text-violet-600 text-black-200 text-5xl font-serif">
             <h1 className='text-6xl'>ACCUEIL</h1>
           </div>
         </div>
       </div>
-      <div class="dark:text-violet-600 text-black-200 font-mono leading-loose ">
+      <div className="dark:text-violet-600 text-black-200 font-mono leading-loose ">
         <p className="italic text-sm text-center">Create, Publish, Discover...</p>
         <div className='text-center mt-9'>
           <p><span className='font-bold text-xl uppercase'>Welcome in your website WebApp !</span><br></br>You can find numerous articles about differents topics such as sports, politics or even health made by you !<br></br>This is a place of freedom of expression where you can share your knowledge with our community and share your opinion about differents subjects.</p>
@@ -26,11 +26,11 @@ export default function Home({ articles }) {
               <div className='object-contain h-24 w-48 mr-6 ml-6 drop-shadow-2xl rounded-md'>
                 <Image src={henriImg} alt="Picture of the author" />
               </div>
-              <p className='text-xl dark:text-violet-600 text-black-200 font-mono leading-loose '>Me, {"It's"} Henri, {"I'm"} a 4th year student at the ECE.<br></br> I like computer development.</p>
+              <p className='text-xl dark:text-violet-600 text-black-200 font-mono leading-loose '>Me, It&apos;s Henri, I&apos;m a 4th year student at the ECE.<br></br> I like computer development.</p>
               <div className='object-contain h-24 w-48 mr-6 ml-6 drop-shadow-2xl rounded-md'>
                 <Image src={erwanImg} alt="Picture of the author" />
               </div>
-              <p className='text-xl dark:text-violet-600 text-black-200 font-mono leading-loose '>Me, {"It's"} Erwan, {"I'm"} a 4th year student at the ECE.<br></br> {"I'm"} passionate about sports and I like computer development.</p>
+              <p className='text-xl dark:text-violet-600 text-black-200 font-mono leading-loose '>Me, It&apos;sErwan, I&apos;m a 4th year student at the ECE.<br></br> I&apos;m passionate about sports and I like computer development.</p>
             </div>
           </div>
         </div>
