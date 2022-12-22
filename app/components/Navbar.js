97926471a69dc22fed96d41777f12dd747461592
erwan1812 <78738801+erwan1812@users.noreply.github.com>
2022-12-22 14:34:28 +0100

    fix: deployment

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -6,7 +6,7 @@ function MobileNav({ open, setOpen }) {
   return (
     <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
       <div className="flex items-center justify-center filter drop-shadow-md bg-white h-20">
-        
+
         <Link href={"/"}>
           <a className="text-xl font-semibold">Accueil</a>
         </Link>
